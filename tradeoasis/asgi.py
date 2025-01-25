251a080ef3e26432345fb85a3bb89e380a72252e
Jagor Obad <jagor.obad@gmail.com>
2025-01-25 20:28:52 +0100

    lal

--- tradeoasis/asgi.py
+++ tradeoasis/asgi.py
@@ -14,3 +14,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tradeoasis.settings")
 
 application = get_asgi_application()
+
+#akdfgsadfasd
+#alalalalalalallala
