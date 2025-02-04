bea53576068982aa4ef3e6fed80f7067ad9d6333
MatejToth <matej.toth021@gmail.com>
2025-02-04 18:25:59 +0100

    Useri dodani u admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom admin panel for managing users.
+    """
+    model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)
