2b062fbc41089b01bc4748fc92104eb16eb6e32f
Jagor Obad <jagor.obad@gmail.com>
2025-03-02 19:11:07 +0100

    new password reset mail

--- tradeoasis/settings.py
+++ tradeoasis/settings.py
@@ -79,14 +79,13 @@ WSGI_APPLICATION = "tradeoasis.wsgi.application"
 
 #Email stuff
 load_dotenv()
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = 'apikey'  
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY') 
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')  
 
 DATABASES = {
     "default": {
